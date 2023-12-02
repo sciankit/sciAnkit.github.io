8961b3353367d11c2293945b390b6ab00b6519a6
ANKIT-ZORIN <bankit12@yahoo.com>
2023-12-02 19:06:22 +0530

    first

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Personal Website v1.0.0 (https://sciankit.in)
+* Copyright 2013-2023 Ankit Bansal
+* Licensed under MIT (https://github.com/StartBootstrap/Ankit Bansal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
