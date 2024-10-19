288f95795b636cadee118b53ded83d0f0a775307
Ankit Bansal <bankit12@yahoo.com>
2024-10-18 22:46:06 -0400

    New_Template

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-/*!
-* Start Bootstrap - Personal Website v1.0.0 (https://sciankit.in)
-* Copyright 2013-2023 Ankit Bansal
-* Licensed under MIT (https://github.com/StartBootstrap/Ankit Bansal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
